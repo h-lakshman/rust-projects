6cf9a923b7a8bfd204bd61af0d46c302c3c82ee9
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-02-11 17:01:03 +0530

    Added retrieving args from cmd, reading a file and printing data in terminal

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,46 @@
 use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let search_query = &args[1];
-    let file_name = &args[2];
-    println!("Searching for {} \nin file {}", search_query, file_name);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguements{err}");
+        process::exit(1)
+    });
+    println!(
+        "Searching for {} \nin file {}",
+        config.search_query, config.file_path
+    );
+    if let Err(e) = run(&config) {
+        println!("Application error {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    search_query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let search_query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config {
+            search_query,
+            file_path,
+        })
+    }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let file_contents =
+        fs::read_to_string(&config.file_path).expect("Should have been able to read the file");
+    println!("Contents: \n{}", file_contents);
+    Ok(())
 }
